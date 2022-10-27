7d05e8e9bd830cf8393f342dca7b91daf149e584
MaldoFred <114544297+MaldoFred@users.noreply.github.com>
2022-10-26 19:04:44 -0500

    realiza laboratorio

--- src/chronometer.js
+++ src/chronometer.js
@@ -4,15 +4,22 @@ class Chronometer {
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) {
+        printTimeCallback(this.currentTime);
+      }
+    }, 1000);
+  
+
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60);
   }
 
   computeTwoDigitNumber(value) {
@@ -20,11 +27,11 @@ class Chronometer {
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    return (this.currentTime = 0);
   }
 
   split() {
